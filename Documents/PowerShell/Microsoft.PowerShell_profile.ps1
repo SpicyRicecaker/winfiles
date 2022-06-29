cf886ffac67ffc4271ccebb7c8674aeb0ac678f6
Andy Li <54951411+SpicyRicecaker@users.noreply.github.com>
2022-06-29 01:23:23 -0700

    f

--- Documents/PowerShell/Microsoft.PowerShell_profile.ps1
+++ Documents/PowerShell/Microsoft.PowerShell_profile.ps1
@@ -3,11 +3,21 @@ Set-PSReadLineOption -EditMode Vi
 Set-PSReadLineKeyHandler -Chord "Ctrl+f" -Function AcceptSuggestion
 Set-PSReadLineKeyHandler -Chord "Ctrl+w" -Function BackwardDeleteWord
 
+# zoxide
+Invoke-Expression (& {
+    $hook = if ($PSVersionTable.PSVersion.Major -lt 6) { 'prompt' } else { 'pwd' }
+    (zoxide init --hook $hook powershell | Out-String)
+})
+
 $global:g = "$HOME\git"
 $global:nvim = "$HOME\AppData\Local\nvim"
 
 function dotgit () {
 	git --git-dir=$HOME/winfiles.git/ --work-tree=$HOME $args
+}
+
+function dotgitlazy () {
+	lazygit --git-dir=$HOME/winfiles.git/ --work-tree=$HOME $args
 }
 
 Invoke-Expression (&starship init powershell)

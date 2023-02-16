832bc63ca7411dcb18a345242e5a6a9b86db486e
SpicyRicecaker <awesomenessisgolden@gmail.com>
2023-02-16 13:54:18 -0800

    new d

--- Documents/PowerShell/Microsoft.PowerShell_profile.ps1
+++ Documents/PowerShell/Microsoft.PowerShell_profile.ps1
@@ -1,6 +1,5 @@
 Set-PSReadLineOption -PredictionSource History
-Set-PSReadLineOption -EditMode Vi
-Set-PSReadLineKeyHandler -Chord "Ctrl+f" -Function AcceptSuggestion
+Set-PSReadLineOption -EditMode Emacs
 Set-PSReadLineKeyHandler -Chord "Ctrl+w" -Function BackwardDeleteWord
 
 # zoxide
@@ -14,14 +13,5 @@ $global:nvim = "$HOME\AppData\Local\nvim"
 function dotgit () {
 	git --git-dir=$HOME/winfiles.git/ --work-tree=$HOME $args
 }
-
-function dotgitlazy () {
-	lazygit --git-dir=$HOME/winfiles.git/ --work-tree=$HOME $args
-}
-
-function lookup () {
-	deno run --allow-net $HOME/git/lexico_scraper/main.ts $args
-}
-
 
 Invoke-Expression (&starship init powershell)
